623ac15abd3614725016d51fc5e6ac9b28e45149
jdmulkey <33848409+jdmulkey@users.noreply.github.com>
2017-11-20 15:58:59 -0900

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,6 +1,13 @@
 (function(window){
   
-  document.getElementById('afhcan-cerner-version').innerHTML = 'VPR SMART FHIR';
+  document.getElementById('afhcan-cerner-version').innerHTML = 'Version: VPR SMART FHIR';
+  
+  // Hide the inline alert whenever the window is clicked on.
+	window.onclick = function(event) {
+		if (event.target.id !== 'inlineAlertDiv' && event.target.id !== 'alertTextSpan' && event.target.id !== 'alertDescriptionSpan') {
+	    	$('#inlineAlertDiv').hide();
+	    }
+	}
   
   window.extractData = function() {
     var ret = $.Deferred();
@@ -11,6 +18,7 @@
     }
 
     function onReady(smart)  {
+      console.log(smart);
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
@@ -28,7 +36,8 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
+          console.log(patient);
+          /*var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
           var dob = new Date(patient.birthDate);
           var day = dob.getDate();
@@ -48,17 +57,17 @@
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          var ldl = byCodes('2089-1');*/
 
           var p = defaultPatient();
-          p.birthdate = dobStr;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.age = parseInt(calculateAge(dob));
-          p.height = getQuantityValueAndUnit(height[0]);
+          //p.birthdate = dobStr;
+          //p.gender = gender;
+          //p.fname = fname;
+          //p.lname = lname;
+          //p.age = parseInt(calculateAge(dob));
+          //p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
+          /*if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
 
@@ -67,7 +76,7 @@
           }
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.ldl = getQuantityValueAndUnit(ldl[0]);*/
 
           ret.resolve(p);
         });
@@ -80,6 +89,74 @@
     return ret.promise();
 
   };
+  
+  function getLink() {
+	getIndirectRoomUrl(copyLinkToClipboardAndReport);
+}
+function copyLinkToClipboardAndReport(link) {
+	copyToClipboard('Click the following link to join your virtual patient room:\r\n' + link);
+	document.getElementById('alertDescriptionSpan').innerHTML = 'Invitation link has been copied to the clipboard.  Use CTRL-V to paste into a message.';
+	document.getElementById('alertTextSpan').innerHTML = link;
+	$('#inlineAlertDiv').slideDown();
+}
+
+function getLinkAndLaunch() {
+	getIndirectRoomUrl(launchLink);
+}
+function launchLink(link) {
+	APPLINK(100, link, '');
+}
+
+function getIndirectRoomUrl(callback) {
+	var sha256 = new Hashes.SHA256;
+	jQuery.support.cors = true;
+	try {
+		$.ajax({
+			url: override.coreUrl + "api/Vidyo/PatientRoomUrl/" + sha256.hex('' + patientId) + "/" + ehrId + "/" + cernerUserId,
+			beforeSend: function(xhr){            
+					var hashStr = "Basic " + Base64.encode(ehrId + ":" + Base64.decode(ehrPassword));
+					xhr.setRequestHeader('Authorization', hashStr);
+			},
+			type: "GET",
+			success: function(link){
+				if (link.length == 0) {
+					alert("Core failed to produce a link!");
+					return;
+				}
+				callback(link);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				alert("Failed to get guest room link from Core. Error: " + errorThrown);
+			}	
+		});
+	}
+	catch(e)
+	{
+		console.log("Connection to core failed :" + e);
+	}
+  
+  function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text); 
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+  
+  
 
   function defaultPatient(){
     return {
@@ -96,59 +173,10 @@
     };
   }
 
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
-
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
-
-  function isLeapYear(year) {
-    return new Date(year, 1, 29).getMonth() === 1;
-  }
-
-  function calculateAge(date) {
-    if (Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime())) {
-      var d = new Date(date), now = new Date();
-      var years = now.getFullYear() - d.getFullYear();
-      d.setFullYear(d.getFullYear() + years);
-      if (d > now) {
-        years--;
-        d.setFullYear(d.getFullYear() - 1);
-      }
-      var days = (now.getTime() - d.getTime()) / (3600 * 24 * 1000);
-      return years + days / (isLeapYear(now.getFullYear()) ? 366 : 365);
-    }
-    else {
-      return undefined;
-    }
-  }
-
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
-    }
-  }
-
   window.drawVisualization = function(p) {
-    $('#holder').show();
+    $('#container').show();
     $('#loading').hide();
-    $('#fname').html(p.fname);
+    /*$('#fname').html(p.fname);
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
@@ -157,7 +185,7 @@
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
+    $('#hdl').html(p.hdl);*/
   };
 
 })(window);
