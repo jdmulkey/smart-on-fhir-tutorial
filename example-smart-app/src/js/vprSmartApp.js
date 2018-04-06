fc03ed0b29fecb8a13ac9281e0c0e09b0a8de53d
jdmulkey <33848409+jdmulkey@users.noreply.github.com>
2018-04-06 08:59:59 -0800

    Update vprSmartApp.js

--- example-smart-app/src/js/vprSmartApp.js
+++ example-smart-app/src/js/vprSmartApp.js
@@ -38,13 +38,13 @@
                 var userId = userIdSections[userIdSections.length - 1];
                 var ur = smart.api.read({ type: userType, id: userId });
                 
-                /*var app = smart.patient.api.fetchAll({
+                var app = smart.patient.api.fetchAll({
                     type: 'Appointment'
-                  });*/
+                  });
 
-                $.when(pt, ur).fail(onError);
-                $.when(pt, ur).done(function (patient, user) {
-                    
+                $.when(pt, ur, app).fail(onError);
+                $.when(pt, ur, app).done(function (patient, user, ap) {
+                    alert(ap)
                     //
                       /*var gender = patient.gender;
                       var dob = new Date(patient.birthDate);
